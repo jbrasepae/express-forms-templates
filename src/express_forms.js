7f7318ef387944c67481c638dbf928acce0313d1
Jeanette <jbrasepae@gmail.com>
2020-09-16 03:13:18 +0200

    connected to database

--- src/express_forms.js
+++ src/express_forms.js
@@ -1,5 +1,29 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("../node_modules/express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app.listen(3000, () => console.log('server started'))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log("server listening on 3000");
+});
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/visitor", (request, response) => {
+  console.log(request.body);
+});
+
+var mongoDB = 'mongodb://127.0.0.1/umuzi';
+//mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;  
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+mongoose.connect(mongoDB, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  
+});
