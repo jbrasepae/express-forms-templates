da422f3fb76ab04007c0ee2edc9534d1751a275d
Jeanette <jbrasepae@gmail.com>
2020-09-18 16:30:36 +0200

    src file completed

--- src/express_forms.js
+++ src/express_forms.js
@@ -1,39 +1,3 @@
-// const express = require('../node_modules/express');
-// const bodyParser = require('../node_modules/body-parser');
-// const app = express();
-// const port = 3000;
-// const path = require('path');
-
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// var mongoose = require('../node_modules/mongoose');
-// var mongoDB = 'mongodb://127.0.0.1/VisitorsDB';
-// mongoose.connect(mongoDB, { useNewUrlParser: true });
-// var db = mongoose.connection;  
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// app.get('/visitor', (request, response) => {
-//   response.sendFile(path.join(__dirname + '/index.html'));
-// });
-
-// app.post('/visitor', (req, res) => {
-//   console.log(req.body)
-//   addNewVisitor(req.body)
-// res.send(req.body);
-// res.render('express'{
-//   name
-// })
-// });
-
-// function addNewVisitor(req){  
-// db.collection("New_Visitor").insertOne(req);
-// }
-
-// app.listen(port, () => console.log(`server running on ://localhost:${port}`));
 const express = require('../node_modules/express');
 const bodyParser = require('../node_modules/body-parser');
 const app = express();
@@ -47,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require('../node_modules/mongoose');
-var mongoDB = 'mongodb://127.0.0.1/UmuziDB';
+var mongoDB = 'mongodb://127.0.0.1/VisitorsDB';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;  
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -77,4 +41,6 @@ function addNewVisitor(req){
 db.collection("New_Visitor").insertOne(req);
 }
 
-app.listen(port, () => console.log(`running on ://localhost:${port}`));
+app.listen(port, () => console.log(`server running on ://localhost:${port}`));
+
+module.exports = {addNewVisitor};
